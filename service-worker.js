c95203d38370e9424b3f1e097f4dc042b192d3dc
WincherZ <wincher2016@gmail.com>
2020-05-06 17:22:36 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.237993ceab8c171497bfb95469a06636.js"
+  "./precache-manifest.483312871926236d5341dcde54720a6a.js"
 );
 
 self.addEventListener('message', (event) => {
